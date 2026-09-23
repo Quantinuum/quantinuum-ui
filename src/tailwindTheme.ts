97d80413cec86496e8945dd7b0d4f718c0d442b3
Sean Burton <sean.burton@quantinuum.com>
2026-09-23 16:41:51 +0100

    Set fonts explicitly back to Tailwind v3 defaults so they remain the same as before

--- src/tailwindTheme.ts
+++ src/tailwindTheme.ts
@@ -24,6 +24,17 @@ export const tailwindTheme = {
       '3xl': '1920px',
     },
     extend: {
+      fontFamily: {
+        sans: [
+          'ui-sans-serif',
+          'system-ui',
+          'sans-serif',
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"',
+        ],
+      },
       colors: {
         border: 'hsl(var(--border))',
         input: 'hsl(var(--input))',
